521d20e3d8dfb93d88950402088d53c80751892c
zengweiqun <zengweiqun@meituan.com>
2016-11-10 16:53:04 +0800

    delete src

--- src/HistoryUtils.js
+++
@@ -1,7 +0,0 @@
-import history from './history';
-
-export const jumpTo = to => history.push(to);
-export const goBack = () => history.goBack();
-export const goForward = () => history.goForward();
-
-export default { jumpTo, goBack, goForward };
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Router from './Router';
-import Route from './Route';
-import Link from './Link';
-import HistoryUtils from './HistoryUtils';
-
-export { Router };
-export { Route };
-export { Link };
-export { HistoryUtils };
